5625689be872fe83913f05c05e30584c12ce688a
jcepriano <jamescepriano@gmail.com>
2023-08-01 12:21:50 -0700

    Done with user story

---
+++ CoffeeShopMVC/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShopMVC.Models;
+using CoffeeShopMVC.DataAccess;
+
+namespace CoffeeShopMVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly CoffeeShopMVCContext _context;
+
+        public CustomersController(CoffeeShopMVCContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            //var customerId = customer.Id;
+
+            return RedirectToAction("index");
+        }
+    }
+}
