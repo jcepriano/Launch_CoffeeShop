895301d75837a6dabf705a8736de1ebc62c5cd65
jkimball54 <jeremyck54@gmail.com>
2023-08-01 11:51:17 -0600

    Added Customers, Orders | Updated Items. IT WORKS

--- CoffeeShopMVC/Models/Item.cs
+++ CoffeeShopMVC/Models/Item.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int PriceInCents { get; set; }
+        public int OrderID { get; set; }
+        public Order Order { get; set; }
     }
 }
