11ce987a636a9ef5594104c9a919a24890812e8d
jkimball54 <jeremyck54@gmail.com>
2023-08-01 13:15:25 -0600

    User story 6 complete

---
+++ CoffeeShopMVC/Controllers/CustomersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShopMVC.DataAccess;
+using CoffeeShopMVC.Models;
+
+namespace CoffeeShopMVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly CoffeeShopMVCContext _context;
+
+        public CustomersController(CoffeeShopMVCContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = _context.Customers.ToList();
+            return View(customers);
+        }
+    }
+}
