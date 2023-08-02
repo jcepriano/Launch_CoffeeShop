19147f8dd71edf0854d5648512c85bb15f7f1b50
jkimball54 <jeremyck54@gmail.com>
2023-08-02 11:25:46 -0600

    WIP

---
+++ CoffeeShopMVC/Controllers/OrdersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShopMVC.Models;
+using CoffeeShopMVC.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace CoffeeShopMVC.Controllers
+{
+    public class OrdersController : Controller
+    {
+
+        public readonly CoffeeShopMVCContext _context;
+
+        public OrdersController(CoffeeShopMVCContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("customers/{id:int}/orders/new")]
+        public IActionResult New(int id)
+        {
+            var customerOrders = _context.Customers.Where(c => c.Id == id).Include(c => c.Orders);
+            return View(customerOrders);
+        }
+
+        [HttpPost]
+        [Route("customers/{id:int}/orders")]
+        public IActionResult Create(Order order, int id)
+        {
+            var customerOrders = _context.Customers.Where(c => c.Id == id).Include(c => c.Orders).FirstOrDefault();
+            customerOrders.Orders.Add(order);
+            _context.SaveChanges();
+
+            return RedirectToAction("show", new { id = customerOrders.Id});
+        }
+    }
+}
