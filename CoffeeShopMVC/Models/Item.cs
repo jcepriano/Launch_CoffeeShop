e3aef51d6e5b295a799b8207f5e4f4bba47526c2
jkimball54 <jeremyck54@gmail.com>
2023-08-02 11:56:18 -0600

    fixed totals in user show page

--- CoffeeShopMVC/Models/Item.cs
+++ CoffeeShopMVC/Models/Item.cs
@@ -7,5 +7,10 @@
         public int PriceInCents { get; set; }
         public Order? Order { get; set; }
         //public List<Order> Orders { get; set; } = new List<Order>();
+
+        public string PriceInDollars()
+        {
+            return string.Format("{0:#.00}", Convert.ToDecimal(PriceInCents) / 100);
+        }
     }
 }
